23ff7b383ecc8cd33ebc745fda4a46dcbffaa94e
dekisa94 <dsimic94@gmail.com>
2018-05-08 18:43:03 +0200

    Task1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">CarsApp</a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link class="nav-link" to="/cars">Cars</router-link>
+      </li>
+    </ul>
+</nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import AppCars from './components/AppCars.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', redirect: '/cars'},
+  {path: '/cars', component: AppCars}
+
+]
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+
 new Vue({
+  router,
+
   render: h => h(App)
 }).$mount('#app')
