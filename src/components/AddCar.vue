026545ba4a98b9e0f3440cea4ba691aba289682e
dekisa94 <dsimic94@gmail.com>
2018-05-08 21:29:02 +0200

    Task6

--- src/components/AddCar.vue
+++ src/components/AddCar.vue
@@ -4,37 +4,36 @@
   <form @submit.prevent="addCar">
     <div class="form-group">
         <label for="brand">Brand</label>
-        <input v-model="newCar.brand" type="text" id="brand" name="brand" />
+        <input v-model="newCar.brand" type="text" id="brand" name="brand" class="form-control" pattern=".{2,}" required title="2 characters minimum"/>
     </div>
     <div class="form-group">
         <label for="model">Model</label>
-        <input v-model="newCar.model" type="text" id="model" name="model" />
+        <input v-model="newCar.model" type="text" id="model" name="model" class="form-control" pattern=".{2,}" required title="2 characters minimum"/>
     </div>
     <div class="form-group">
         <label for="year">Year</label>
-        <select v-model="newCar.year" id="year" name="year">
+        <select v-model="newCar.year" id="year" name="year" class="form-control" required>
             <option value="1990">1990</option>
             <option value="2018">2018</option>
         </select>
     </div>
     <div class="form-group">
         <label for="maxSpeed">Max speed</label>
-        <input v-model="newCar.maxSpeed" type="number" id="maxSpeed" name="maxSpeed" />
+        <input v-model="newCar.maxSpeed" type="number" id="maxSpeed" name="maxSpeed" class="form-control" required/>
     </div>
     <div class="form-group">
         <label for="numberOfDoors">Number of doors</label>
-        <input v-model="newCar.numberOfDoors" type="number" id="numberOfDoors" name="numberOfDoors" />
+        <input v-model="newCar.numberOfDoors" type="number" id="numberOfDoors" name="numberOfDoors" class="form-control" required/>
     </div>
     <div class="form-group">
-            <label>Transmission</label>
             <div>
-                <input v-model="newCar.isAutomatic" type="checkbox" id="automatic" name="isAutomatic" value="true">
                 <label for="automatic">Automatic</label>
+                <input v-model="newCar.isAutomatic" type="checkbox" id="automatic" name="isAutomatic" value="true">
             </div>
     </div>
     <div class="form-group">
         <label>Engine type</label><br/>
-        <input v-model="newCar.engine" type="radio" name="engine" value="diesel"> Diesel<br>
+        <input v-model="newCar.engine" type="radio" name="engine" value="diesel" > Diesel<br>
         <input v-model="newCar.engine" type="radio" name="engine" value="petrol"> Petrol<br>
         <input v-model="newCar.engine" type="radio" name="engine" value="electric"> Electric<br>
         <input v-model="newCar.engine" type="radio" name="engine" value="hybrid"> Hybrid
